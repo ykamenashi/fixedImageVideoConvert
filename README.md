30f2011b57ae29a3ef2e8ced939997da9b547cd8
ykamenashi <kamenashi@cloud-ace.jp>
2021-01-21 20:45:25 +0900

    [1st] vaperware

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fixedImageVideoConvert
 output video file for YouTube with ffmpeg. ( depending )
+
+## Correctly working checked ffmpeg version
+* ffmpeg version 4.3.1
---
+++ vidconv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+### ffmpeg existing check
+
+### input file existing check
+
+### output file not existing check
+
+### convert process
+
+### notify to user ( use bell )
+
+### end section
+
+exit 0
